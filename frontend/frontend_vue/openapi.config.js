d2a543a828a42d65d30edf08929a347937306a36
Caelan_Mac <caelan123456@outlook.jp>
2025-11-13 10:28:42 +0800

    删除前端vue代码

--- frontend/frontend_vue/openapi.config.js
+++
@@ -1,7 +0,0 @@
-import { generateService } from '@umijs/openapi'
-
-generateService({
-  requestLibPath: "import request from '@/request'",
-  schemaPath: 'http://localhost:8000/openapi.json',
-  serversPath: './src',
-})
--- frontend/frontend_vue/src/pages/GraphPage.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div id="GraphPage">
-
-  </div>
-</template>
